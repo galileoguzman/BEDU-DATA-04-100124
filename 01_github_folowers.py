df2ae4070bd057681936689871b29b5085e80fbb
galileoguzman <galileoguzman@gmail.com>
2024-01-10 20:48:54 -0600

    create function to get user followers and download their images

--- 01_github_folowers.py
+++ 01_github_folowers.py
@@ -22,7 +22,20 @@ def download_avatar_user(avatar_url, username):
             return filename
     return None
 
+def get_user_folowers(username):
+    url = f'{BASE_URL}users/{username}/followers'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
 
 username = input('Give me an username you want to extract the information:\n')
 selected_user = get_github_user(username)
-print(download_avatar_user(selected_user.get('avatar_url'), username))
+
+user_folowers = get_user_folowers(username)
+
+for follower in user_folowers:
+    folower_avatar_url = follower.get('avatar_url')
+    folower_username = follower.get('login')
+    download_avatar_user(folower_avatar_url, folower_username)
+
